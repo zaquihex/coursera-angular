27b70d3bde26dafeef9ab3a53d5d8bcccfd90864
zaquiel.rodriguez.arce <zaquihex@gmail.com>
2020-11-02 21:58:38 +0100

    services and routing added

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -7,19 +7,40 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { DishDetailComponent } from './dish-detail/dish-detail.component';
+import {DishService} from "./services/dish.service";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from "./app-routing/app-routing.module";
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatListModule
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ conFusion/src/app/dish-detail/dish-detail.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { Dish } from '../shared/dish';
+
+@Component({
+  selector: 'app-dish-detail',
+  templateUrl: './dish-detail.component.html',
+  styleUrls: ['./dish-detail.component.scss']
+})
+export class DishDetailComponent implements OnInit {
+
+
+
+  @Input()
+  dish: Dish;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
